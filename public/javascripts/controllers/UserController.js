2b5fa5946bc04368cb993524d13e22a44746b9e8
Gustavo Ferreira <gustavogardel@gmai.com>
2016-09-03 19:41:40 -0700

    auth with login, logout and register

---
+++ public/javascripts/controllers/UserController.js
@@ -0,0 +1,33 @@
+angular.module("inclusion_app")
+	.controller("UserController", UserController)
+
+UserController.$inject = ['$http', '$state']
+
+function UserController ($http, $state) {
+	var self = this
+
+	self.username = ''
+
+	$http({
+		url: '/user',
+		method: 'GET',
+		params: {
+			token: localStorage.token
+		}
+	}).then(function (response) {
+		console.log(response)
+		self.username = response.data.username
+
+		// stay away hackers!
+		if (response.data.message === "jwt malformed")
+			window.location = "https://c7.staticflickr.com/9/8166/29057776390_74fcea1b5e_o.jpg"
+	})
+
+
+	this.logout = function(){
+	localStorage.removeItem('token')
+	$state.go('login')
+	}
+
+
+}
